52a433f85e456729f13e0d12943d368509f8d0ac
Greg <greg>
2019-11-08 23:14:39 -0800

    make sure to config searchPath

--- backend/knex.js
+++ backend/knex.js
@@ -1,8 +1,8 @@
 const knex = require('knex');
 
-module.exports = function (app) {
-  const { client, connection } = app.get('postgres');
-  const db = knex({ client, connection });
+module.exports = function(app) {
+  const { client, connection, searchPath } = app.get('postgres');
+  const db = knex({ client, connection, searchPath });
 
   app.set('knexClient', db);
 };
