602dfc6ae62688586adb4b1fb9243a21705a80b8
Greg <greg>
2023-12-14 19:04:56 -0800

    serve nodigital from app

--- frontend/src/components/Thumbnail.jsx
+++ frontend/src/components/Thumbnail.jsx
@@ -5,26 +5,30 @@ import { BrokenImage } from '@mui/icons-material';
 import React from 'react'
 
 const onError = (event) => {
-  event.target.src = `https://search.freedomarchives.org/images/fileicons/nodigital.png`
-}
+  event.target.src = `/static/images/nodigital.png`;
+};
 
-export default function Thumbnail({ src, type, width = 75, alt = '' }) {
+export default function Thumbnail({ src, type, width = 75, alt = "" }) {
   if (src) {
     return (
-      <span style={{ width, minWidth: width, display: 'block', marginRight: 10 }}>
+      <span
+        style={{ width, minWidth: width, display: "block", marginRight: 10 }}
+      >
         <img
           style={{
-            maxWidth: '100%',
+            maxWidth: "100%",
           }}
           src={src}
           onError={onError}
           alt={alt}
         />
       </span>
-    )
+    );
   } else {
-    return <Avatar style={{ width, height: width }}>
-      <BrokenImage style={{ fontSize: width * 0.6 }} />
-    </Avatar>
+    return (
+      <Avatar style={{ width, height: width }}>
+        <BrokenImage style={{ fontSize: width * 0.6 }} />
+      </Avatar>
+    );
   }
 }
