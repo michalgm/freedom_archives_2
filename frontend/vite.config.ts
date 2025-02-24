542dab57637211de27327cc8dc533a04de4014f8
Greg Michalec <greg+github@primate.net>
2025-02-23 23:46:16 -0800

    remove analyzer from vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { analyzer } from 'vite-bundle-analyzer'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
     // depending on your application, base can also be "/"
     base: '',
-  plugins: [react(), viteTsconfigPaths(), analyzer()],
+  plugins: [react(), viteTsconfigPaths()],
     server: {    
         port: 4040, 
         proxy: {
