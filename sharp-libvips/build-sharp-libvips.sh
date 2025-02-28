24418001f1562ad601ea06ac92a92470d0e4ec26
Greg Michalec <greg+github@primate.net>
2025-02-27 16:27:33 -0800

    Add tooling to build custom libvips to enable pdf support in sharp Add npm dependency on custom sharp-libvips package

---
+++ sharp-libvips/build-sharp-libvips.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+REPO=https://github.com/lovell/sharp-libvips
+RELEASE_TAG=$(curl -s "https://api.github.com/repos/lovell/sharp-libvips/releases/latest" | jq -r .tag_name)
+rm -rf sharp-libvips
+
+git clone --depth 1 --branch $RELEASE_TAG "${REPO}"
+cd sharp-libvips
+git apply ../pdfium.patch
+
+export VERSION_LATEST_REQUIRED=false
+LIBVIPS_VERSION=$(cat LIBVIPS_VERSION)
+./build.sh ${LIBVIPS_VERSION} linux-x64
+tar -xzvf "./libvips-${LIBVIPS_VERSION}-linux-x64.tar.gz" -C ./npm/linux-x64
+cd ../
+npm pack ./sharp-libvips/npm/linux-x64
+rm -rf sharp-libvips
+
+
