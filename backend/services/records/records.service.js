7f067bdd7d7b982d8f065cee46f85b408775bade
Greg Michalec <greg+github@primate.net>
2025-03-24 15:33:33 -0700

    fix records search

--- backend/services/records/records.service.js
+++ backend/services/records/records.service.js
@@ -2,28 +2,47 @@
 const { KnexService } = require("@feathersjs/knex");
 const hooks = require("./records.hooks");
 
-class Records extends KnexService {
-  constructor(options) {
-    super({
-      ...options,
-      name: "records",
-      filters: {
-        $fullText: (v) => v,
-      },
-    });
-  }
-}
+class Records extends KnexService {}
+
+const getOptions = (app) => ({
+  Model: app.get("postgresqlClient"),
+  paginate: app.get("paginate"),
+  name: "records",
+  id: "record_id",
+  multi: true,
+  operators: ["$fullText", "$contains"],
+  filters: {
+    $fullText: (v) => v,
+    $contains: (v) => v,
+  },
+});
+//   constructor(options) {
+//     super({
+//       ...options,
+//       // filters: {
+//       //   $fullText: (v) => v,
+//       //   $contains: (v) => v,
+//       // },
+//       operators: ["$fullText", "$contains"],
+//     });
+//   }
+// }
 
 module.exports = function (app) {
-  const options = {
-    id: "record_id",
-    Model: app.get("postgresqlClient"),
-    paginate: app.get("paginate"),
-    multi: true,
-  };
+  // const options = {
+  //   id: "record_id",
+  //   Model: app.get("postgresqlClient"),
+  //   paginate: app.get("paginate"),
+  //   multi: true,
+  //   // filters: {
+  //   //   $fullText: (v) => v,
+  //   //   $contains: (v) => v,
+  //   // },
+  //   operators: ["$fullText", "$contains"],
+  // };
 
   // Initialize our service with any options it requires
-  app.use("/api/records", new Records(options, app));
+  app.use("/api/records", new Records(getOptions(app)));
 
   // Get our initialized service so that we can register hooks
   const service = app.service("api/records");
