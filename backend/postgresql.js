30489923921b67d147dc934ead1991401fbed68b
Greg Michalec <greg+github@primate.net>
2025-03-24 15:08:35 -0700

    add postgres extensions

--- backend/postgresql.js
+++ backend/postgresql.js
@@ -4,6 +4,12 @@ module.exports = function (app) {
   const config = app.get("postgresql");
   const db = knex({
     ...config,
+    extendedOperators: {
+      $overlap: "&&",
+      $contains: "@>",
+      $contained_by: "<@",
+      $fulltext: "@@",
+    },
     // debug: true,
   });
 
