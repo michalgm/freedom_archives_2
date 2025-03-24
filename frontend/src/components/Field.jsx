af99779074a2be16e369eceed8e0ed076cb3aa57
Greg Michalec <greg+github@primate.net>
2025-03-24 16:36:56 -0700

    fix error display

--- frontend/src/components/Field.jsx
+++ frontend/src/components/Field.jsx
@@ -70,7 +70,7 @@ const FieldComponent = ({
   const variant = props.variant || ro ? "filled" : "outlined";
   let field;
   delete props.raw;
-  const error = Boolean(errors?.name);
+  const error = Boolean(errors?.[name]);
   helperText += error ? errors[name] : "";
   props.onChange = React.useCallback(
     async (event, option) => {
