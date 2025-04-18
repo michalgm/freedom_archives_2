b40aa1d013a7d2e0daf67a38a3bec56a362f1026
Greg Michalec <greg+github@primate.net>
2025-04-18 16:16:28 -0700

    no wdyr

--- frontend/src/wdyr.js
+++ frontend/src/wdyr.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-if (import.meta.env.MODE === "development") {
-  const whyDidYouRender = require("@welldone-software/why-did-you-render");
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-  });
-}
+// if (import.meta.env.__WDYR_ENABLED__) {
+//   const whyDidYouRender = require("@welldone-software/why-did-you-render");
+//   whyDidYouRender(React, {
+//     // include: [/.*/],
+//     // exclude: [/^BrowserRouter/, /^Link/, /^Route/],
+//     // trackHooks: true,
+//     trackAllPureComponents: true,
+//   });
+//   console.log("Why Did You Render initialized");
+// }
