3132e1f7496fbc112f95ba3c26d0bf06c4731c8b
Greg Michalec <greg+github@primate.net>
2025-04-18 13:19:39 -0700

    fix knexfile export

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/databases.html
 import app from "./backend/app.js";
 // Load our database connection info from the app configuration
-export default config = app.get("postgresql");
+const config = app.get("postgresql");
+export default config;
