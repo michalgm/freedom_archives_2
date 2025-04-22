11e15efa207aaee69ca8671f35a0266fb6f22b3f
Greg Michalec <greg+github@primate.net>
2025-04-22 01:55:19 -0700

    feat: refactor ReviewChanges form to use react-hook-form-mui
    This commit refactors the `ReviewChangesForm` component to use `react-hook-form-mui` for improved form management and validation.
    
    Key changes include:
    
    - Replacing the custom `Form` and `Field` components with `FormContainer` and `Field` from `react-hook-form-mui`.
    - Using `useForm` hook to manage form state and validation.
    - Replacing the `filter` prop with `getValues` and `watch` from `useForm` to access form values.
    - Implementing `useDeepCompareEffect` to update the `filter` state when form values change.
    - Removing the `initialValues` prop and using `defaultValues` in `useForm` instead.
    - Adjusting the `customLabel` component to work with `react-hook-form-mui`.
    - Adding `returnFullObject` and `service` props to the `collection_id` and `user_id` fields to configure the select components.
    - Replacing `debounce` prop with `useDebouncedCallback` to debounce the `fetchValues` function.
    - Removing unnecessary `useEffect` hook for setting the initial pagination.
    - Updating the `fetchValues` function to use `useDebouncedCallback`.
    - Adding `highlightDirty={false}` to form fields to prevent highlighting.

--- frontend/src/views/ReviewChanges.jsx
+++ frontend/src/views/ReviewChanges.jsx
@@ -1,30 +1,32 @@
 import { Box, Button, Grid2, Paper, Stack, Typography } from "@mui/material";
 import dayjs from "dayjs";
-import { isEqual, startCase } from "lodash-es";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { startCase } from "lodash-es";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { FormContainer, useForm } from "react-hook-form-mui";
 import { Section } from "src/components/ViewContainer";
+import { useDebouncedCallback } from "use-debounce";
+import useDeepCompareEffect from "use-deep-compare-effect";
 
 import { collections, records, review_changes, snapshots } from "../api";
 import EditableDataTable from "../components/EditableDataTable";
+import { Field } from "../components/form/Field";
 import Link from "../components/Link";
-import Field from "../components/OldField";
-import Form from "../components/OldForm";
 
-const defaultFilter = {
+const defaultValues = {
   needs_review: true,
   date_modified: null,
   type: "all",
   action: "all",
-  user: null,
+  user_id: null,
   title: null,
-  collection: null,
+  collection_id: null,
   changes_since: "published",
   since_amount: 10,
   since_unit: "days",
 };
 
 const createQuery = (filter, publishDate) => {
-  const { title, needs_review, type, collection = {}, action, user } = filter;
+  const { title, needs_review, type, collection_id, action, user_id } = filter;
   const date_modified =
     filter.changes_since === "published" ? publishDate : dayjs().subtract(filter.since_amount, filter.since_unit);
   const query = {
@@ -32,8 +34,8 @@ const createQuery = (filter, publishDate) => {
     date_modified: date_modified ? { $gte: date_modified.format() } : undefined,
     type: type !== "all" ? type : undefined,
     action: action !== "all" ? action : undefined,
-    collection_id: collection?.collection_id ? collection.collection_id : undefined,
-    contributor_user_id: user?.user_id ? user.user_id : undefined,
+    collection_id: collection_id || undefined,
+    contributor_user_id: user_id || undefined,
     title: title ? { $ilike: `%${title}%` } : undefined,
   };
 
@@ -45,13 +47,14 @@ const initialPage = { skip: 0, limit: 15 };
 const fields = [
   {
     name: "needs_review",
-    type: "checkbox",
+    field_type: "checkbox",
     label: "Only changes needing review",
+    labelProps: { sx: { textAlign: "left", userSelect: "none" } },
   },
   {
     name: "date_modified",
     label: "Changes since",
-    type: "datetime",
+    field_type: "datetime",
     slotProps: {
       field: { clearable: true },
     },
@@ -62,22 +65,28 @@ const fields = [
   },
   { name: "title" },
   {
-    name: "collection",
+    name: "collection_id",
     label: "Collection",
-    type: "select",
-    searchType: "collections",
+    field_type: "select",
+    returnFullObject: false,
+    service: "collections",
   },
+
   {
-    name: "user",
+    name: "user_id",
     label: "User",
-    type: "select",
-    searchType: "users",
+    field_type: "select",
+    returnFullObject: false,
+    service: "users",
   },
   {
     name: "type",
     label: "Type",
-    type: "simpleSelect",
-    disableClearable: true,
+    field_type: "select",
+    returnFullObject: false,
+    autocompleteProps: {
+      disableClearable: true,
+    },
     options: [
       { id: "all", label: "All" },
       { id: "collection", label: "Collection" },
@@ -88,7 +97,8 @@ const fields = [
     name: "action",
     label: "Action",
     disableClearable: true,
-    type: "simpleSelect",
+    returnFullObject: false,
+    field_type: "select",
     options: [
       { id: "all", label: "All" },
       { id: "create", label: "Create" },
@@ -97,91 +107,117 @@ const fields = [
   },
 ];
 
-function ReviewChangesForm({ filter, setFilter, publishDate }) {
-  const initialValues = filter.date_modified ? filter : {};
+const ReviewChangesForm = React.memo(function ({ setFilter, publishDate }) {
+  const formContext = useForm({
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const { getValues, watch } = formContext;
+
+  const data = watch();
+
+  useDeepCompareEffect(() => {
+    setFilter(data);
+  }, [data, setFilter]);
+
   const customLabel = (
-    <Stack direction={"row"} spacing={1} alignItems="center" sx={{ width: "251px" }}>
-      <Field
-        type="number"
-        name="since_amount"
-        fullWidth={false}
-        label=" "
-        disabled={filter.changes_since === "published"}
-      />
-      <Field
-        type="simpleSelect"
-        name="since_unit"
-        options={["days", "weeks", "months", "years"]}
-        fullWidth={true}
-        disableClearable
-        disabled={filter.changes_since === "published"}
-      />
-      <span>ago</span>
+    <Stack direction={"row"} spacing={1} alignItems="center" justifyContent={"space-between"}>
+      <Grid2 size={4} sx={{ maxWidth: "100px" }}>
+        <Field
+          field_type="number"
+          name="since_amount"
+          margin="none"
+          highlightDirty={false}
+          label=""
+          disabled={getValues("changes_since") === "published"}
+          // size={{ xs: 6 }}
+          // sx={{ width: 1 / 2 }}
+        />
+      </Grid2>
+      <Grid2 size={7}>
+        <Field
+          field_type="select"
+          name="since_unit"
+          label=""
+          options={["days", "weeks", "months", "years"]}
+          margin="none"
+          disableClearable
+          returnFullObject={false}
+          highlightDirty={false}
+          disabled={getValues("changes_since") === "published"}
+          // sx={{ width: 2 / 3 }}
+          // size={6}
+
+          // sx={{ flex: "1 1 auto" }}
+        />
+      </Grid2>
+      <Grid2 size="auto">
+        <span>ago</span>
+      </Grid2>
     </Stack>
   );
   if (!publishDate) {
     return;
   }
+
   return (
     <Box>
-      <Form
-        initialValues={initialValues}
-        enableReinitialize
-        debounce={300}
-        onChange={(values) => {
-          if (!isEqual(values, filter)) {
-            if (Object.keys(values).length) {
-              setFilter(values);
-            }
-          }
+      <FormContainer
+        formContext={formContext}
+        FormProps={{
+          autoComplete: "off",
         }}
       >
-        {fields.map((field) => {
-          if (field.name === "date_modified") {
-            return (
-              <Grid2 key="date_modified" size={{ xs: 12, md: 9 }}>
-                <Field
-                  type="radiogroup"
-                  row
-                  name="changes_since"
-                  options={[
-                    {
-                      value: "published",
-                      label: (
-                        <>
-                          Last Publish Date{" "}
-                          <Typography variant="caption">({publishDate.format("YYYY-MM-DD hh:mm A")})</Typography>
-                        </>
-                      ),
-                    },
-                    { value: "custom", label: customLabel },
-                  ]}
-                />
-              </Grid2>
-            );
-          } else {
-            return (
-              <Grid2 key={field.name} size={{ xs: 12, md: 2.4 }}>
-                <Field {...field} />
-              </Grid2>
-            );
-          }
-        })}
-      </Form>
+        <Grid2 container spacing={2}>
+          {fields.map((field) => {
+            if (field.name === "date_modified") {
+              return (
+                <Grid2 key="date_modified" size={{ xs: 12, md: 9.6 }} direction={"row"} textAlign={"left"}>
+                  <Field
+                    field_type="radio_group"
+                    name="changes_since"
+                    row
+                    options={[
+                      {
+                        id: "published",
+                        label: (
+                          <>
+                            Last Publish Date{" "}
+                            <Typography variant="caption">({publishDate.format("YYYY-MM-DD hh:mm A")})</Typography>
+                          </>
+                        ),
+                      },
+                      { id: "custom", label: customLabel },
+                    ]}
+                  />
+                </Grid2>
+              );
+            } else {
+              return (
+                <Grid2 key={field.name} size={{ xs: 12, md: 2.4 }}>
+                  <Field {...field} highlightDirty={false} fullWidth />
+                </Grid2>
+              );
+            }
+          })}
+        </Grid2>
+      </FormContainer>
     </Box>
   );
-}
+});
 
 function ReviewChanges() {
   const [values, setValues] = useState({ data: [], total: 0 });
-  const [filter, setFilter] = useState(defaultFilter);
   const [order, setOrder] = useState(initialOrder);
   const [pagination, setPagination] = useState(initialPage);
   const [loading, setLoading] = useState(true);
   const [publishDate, setPublishDate] = useState(null);
+  const [filter, setFilter] = useState(defaultValues);
+
   useEffect(() => {
     setPagination(initialPage);
-  }, [order, filter]);
+  }, [order]);
 
   useEffect(() => {
     const fetchPublishDate = async () => {
@@ -190,43 +226,42 @@ function ReviewChanges() {
           (await snapshots.find({ query: { is_live: true, $select: ["date_published"] } }))[0] || {};
         const date = dayjs(date_published);
         setPublishDate(date);
-        setFilter((f) => ({ ...f, date_modified: date }));
+        // setFilter((f) => ({ ...f, date_modified: date }));
       }
     };
     fetchPublishDate();
   }, [publishDate]);
 
-  const fetchValues = useCallback(async () => {
-    setLoading(true);
-    const query = createQuery(filter, publishDate);
-    const { field = "date_modified", sort = "asc" } = order || {};
-    const $sort = { [field || "date_modified"]: sort === "desc" ? 0 : 1 };
-    if (field !== "item") {
-      $sort.date_modified = sort === "desc" ? 0 : 1;
-    }
-    const values = await review_changes.find({
-      query: {
-        ...query,
-        $limit: pagination.limit,
-        $skip: pagination.skip,
-        $sort,
-      },
-    });
-    const updatedValues = values.data.map((item) => {
-      item.type = startCase(item.type);
-      item.action = startCase(item.action);
-      return item;
-    });
+  // console.log(values);
+  const fetchValues = useDebouncedCallback(
+    useCallback(async () => {
+      setLoading(true);
+      const query = createQuery(filter, publishDate);
+      const { field = "date_modified", sort = "asc" } = order || {};
+      const $sort = { [field || "date_modified"]: sort === "desc" ? 0 : 1 };
+      if (field !== "item") {
+        $sort.date_modified = sort === "desc" ? 0 : 1;
+      }
+      const values = await review_changes.find({
+        query: {
+          ...query,
+          $limit: pagination.limit,
+          $skip: pagination.skip,
+          $sort,
+        },
+      });
+      const updatedValues = values.data.map((item) => {
+        item.type = startCase(item.type);
+        item.action = startCase(item.action);
+        return item;
+      });
 
-    setValues({ ...values, data: updatedValues });
-    setLoading(false);
-  }, [filter, order, pagination.limit, pagination.skip, publishDate]);
-
-  useEffect(() => {
-    if (publishDate) {
-      fetchValues();
-    }
-  }, [publishDate, fetchValues]);
+      setValues({ data: updatedValues, total: values.total });
+      setPagination((prevPagination) => ({ ...prevPagination, page: pagination.page + 1 }));
+      setLoading(false);
+    }, [filter, order, pagination.limit, pagination.page, pagination.skip, publishDate]),
+    300
+  );
 
   const updateReview = useCallback(
     async (id, type, needs_review) => {
@@ -286,6 +321,9 @@ function ReviewChanges() {
     ],
     [updateReview]
   );
+  useEffect(() => {
+    fetchValues();
+  }, [filter, fetchValues, order, pagination.skip]);
 
   const handleSortModelChange = ([order]) => {
     setOrder(order);
@@ -301,10 +339,11 @@ function ReviewChanges() {
         elements={[
           <ReviewChangesForm
             key={"form"}
-            filter={filter}
+            // filter={filter}
             setFilter={setFilter}
+            // fetchValues={fetchValues}
             publishDate={publishDate}
-            setPublishDate={setPublishDate}
+            // setPublishDate={setPublishDate}
           />,
         ]}
       />
