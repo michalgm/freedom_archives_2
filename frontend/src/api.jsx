cd4271e40fb54966f24fe642770b6d0f31df1a2a
Greg Michalec <greg+github@primate.net>
2025-05-29 06:00:28 -0700

    feat: expose service IDs and add service constants
    This commit introduces constants for each service and a function to derive the ID from a service.
    
    - Adds constants for each service defined in `api.jsx`.
    - Introduces a `getServiceID` function that takes a service name and returns the corresponding ID (e.g., `record_id` for the `records` service).
    - This change provides a centralized and consistent way to access service IDs, which can be used throughout the application.

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -23,17 +23,30 @@ app.configure(restClient.fetch(window.fetch.bind(window), MyFetchClient));
 app.configure(auth({ path: "/api/authentication" }));
 
 export const records = app.service("/api/records");
+
 export const relationships = app.service("/api/relationships");
+
 export const authentication = app.service("/api/authentication");
+
 export const list_items = app.service("/api/list_items");
+
 export const collections = app.service("/api/collections");
+
 export const value_lookup = app.service("/api/value_lookup");
+
 export const users = app.service("/api/users");
+
 export const list_items_lookup = app.service("/api/list_items_lookup");
+
 export const review_changes = app.service("/api/review_changes");
+
 export const public_records = app.service("/api/public_records");
+
 export const snapshots = app.service("/api/snapshots");
+
 export const settings = app.service("/api/settings");
+
+export const getServiceID = (service) => `${service.replace(/s$/, "")}_id`;
 
 export const authenticate = async (username, password) => {
   return app
@@ -50,6 +63,7 @@ export const authenticate = async (username, password) => {
 };
 
 export const reAuth = app.reAuthenticate;
+
 export const services = {
   records,
   relationships,
