4a4c0d93f0b2328e4bb32c7461e2048835bb63f1
Greg Michalec <greg+github@primate.net>
2025-06-05 17:40:34 -0700

    allow embedded notifications

--- frontend/src/components/Notifications.jsx
+++ frontend/src/components/Notifications.jsx
@@ -1,7 +1,7 @@
 import { Alert, Snackbar, Stack } from "@mui/material";
 import { useAppStore, useRemoveNotification } from "src/stores";
 
-function Notification({ severity = "success", message, onClose }) {
+function Notification({ severity = "success", embedded, message, onClose }) {
   return (
     <Snackbar
       open={true}
@@ -11,30 +11,40 @@ function Notification({ severity = "success", message, onClose }) {
       disableWindowBlurListener={true}
       sx={{ position: "relative", top: "unset !important" }}
     >
-      <Alert severity={severity} onClose={onClose} elevation={6} sx={{ pointerEvents: "auto" }}>
+      <Alert
+        severity={severity}
+        onClose={onClose}
+        elevation={embedded ? 0 : 6}
+        sx={{ pointerEvents: "auto", flexGrow: embedded ? 1 : undefined }}
+      >
         {message}
       </Alert>
     </Snackbar>
   );
 }
 
-function Notifications() {
+function Notifications({ embedded = false }) {
   const notifications = useAppStore((state) => state.notifications);
   const removeNotification = useRemoveNotification();
-
+  const sx = embedded
+    ? { mb: 2, mt: 0 }
+    : {
+        position: "absolute",
+        top: 8,
+        zIndex: 10000000,
+        left: 24,
+        right: 24,
+        pointerEvents: "none",
+      };
   return (
-    <Stack
-      className="notifications"
-      direction="column"
-      spacing={2}
-      sx={{ position: "absolute", top: 8, zIndex: 10000, left: 24, right: 24, pointerEvents: "none" }}
-    >
+    <Stack className="notifications" direction="column" spacing={2} sx={sx}>
       {notifications.map(({ id, severity, message }, index) => (
         <Notification
           key={id}
           index={index}
           severity={severity}
           message={message}
+          embedded={embedded}
           onClose={(_, reason) => {
             if (!["clickaway", "escapeKeyDown"].includes(reason)) {
               removeNotification(id);
