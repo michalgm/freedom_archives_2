cc618ba1e19c8044070d2594c11c0bb7b5c1417f
Greg Michalec <greg+github@primate.net>
2025-10-31 17:33:28 -0700

    Add search help dialog to SearchForm component

--- frontend/src/layouts/PublicLayout.jsx
+++ frontend/src/layouts/PublicLayout.jsx
@@ -187,58 +187,6 @@ const PublicLayout = () => {
           }}
           disableGutters
         >
-          {/* <div id="help_info">
-            <h2>Search Help</h2>
-            <div id="help_contents">
-              <dl>
-                <dt>How does this work?</dt>
-                <dd>
-                  There are many ways to search the collections of the Freedom Archives. Below is a brief guide that
-                  will help you conduct effective searches. Note, anytime you search for anything in the Freedom
-                  Archives, the first results that appear will be our digitized items. Information for items that have
-                  yet to be scanned or yet to be digitized can still be viewed, but only by clicking on the{" "}
-                  <a href="#">show</a> link that will display the hidden (non-digitized) items. If you are interested in
-                  accessing these non-digitized materials, please email{" "}
-                  <a href="mailto:info@freedomarchives.org">info@freedomarchives.org</a>.
-                </dd>
-                <dt>Exploring the Collections without the Search Bar</dt>
-                <dd>
-                  Under the heading Browse By Collection, you’ll notice most of the Freedom Archives’ major collections.
-                  These collections have an image as well as a short description of what you’ll find in that collection.
-                  Click on that image to instantly explore that specific collection.
-                </dd>
-                <dt>Basic Searching</dt>
-                <dd>
-                  You can always type what you’re looking for into the search bar. Certain searches may generate
-                  hundreds of results, so sometimes it will help to use quotation marks to help narrow down your
-                  results. For instance, searching for the phrase Black Liberation will generate all of our holdings
-                  that contain the words Black and Liberation, while searching for “Black Liberation” (in quotation
-                  marks) will only generate our records that have those two words next to each other.
-                </dd>
-                <dt>Advanced Searching</dt>
-                <dd>
-                  The Freedom Archives search site also understands Boolean search logic, specifcally AND/+, NOT/-, and
-                  OR operators. Click on{" "}
-                  <a target="_new" href="http://libguides.mit.edu/c.php?g=175963&p=1158594">
-                    this link
-                  </a>{" "}
-                  for a brief tutorial on how to use Boolean search logic. Our search function also understands “fuzzy
-                  searches.” Fuzzy searches utilize the (*) and will find matches even when users misspell words or
-                  enter in only partial words for the search. For example, searching for liber* will produce results for
-                  liberation/liberate/liberates/etc.
-                </dd>
-                <dt>Keyword Searches</dt>
-                <dd>
-                  You’ll notice that under the heading KEYWORDS, there are a number of words, phrases or names that
-                  describe content. Sometimes these are also called “tags.” Clicking on these words is essentially the
-                  same as conducting a basic search.
-                </dd>
-              </dl>
-            </div>
-            <button onclick="$.modal.close();">Close</button>
-          </div> */}
-          {/* <Notifications /> */}
-          {/* <Box flexGrow={1} overflow={"auto"} maxHeight={600}> */}
           <Box
             sx={{
               display: "flex",
