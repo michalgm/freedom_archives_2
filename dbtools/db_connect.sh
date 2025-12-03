6a51e12a857b998ab475b8a41e139f9218aefcd4
Greg Michalec <greg+github@primate.net>
2025-12-03 04:26:09 -0800

    update db_connect script

--- dbtools/db_connect.sh
+++ dbtools/db_connect.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-export PGOPTIONS=--search_path=freedom_archives
+ENV=${1:-default}
+if [ -n "$1" ]; then
+  ENV=$1
+  shift
+else
+  ENV=default
+fi
+
+if [ "$ENV" == "p" ] || [ "$ENV" == "prod" ]; then
+  ENV="production"
+fi
+
+if [ "$ENV" != "default" ] && [ "$ENV" != "production" ]; then
+  echo "Unknown environment: $ENV"
+  exit 1
+fi
+source ./read_pg_config.sh $ENV
+
 psql "$@"
