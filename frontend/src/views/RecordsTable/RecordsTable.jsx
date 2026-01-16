66d5b2b79928d2eba02f024d6e858747d08923d0
Greg Michalec <greg+github@primate.net>
2026-01-15 18:52:14 -0800

    feat: refactor RecordsTable component to improve structure and add collection selection

--- frontend/src/views/RecordsTable/RecordsTable.jsx
+++ frontend/src/views/RecordsTable/RecordsTable.jsx
@@ -1,40 +1,50 @@
-import { Button, Link } from "@mui/material";
-import { Box } from "@mui/system";
+import { Link } from "@mui/material";
+import { Box, Stack } from "@mui/system";
 import { useGridApiRef } from "@mui/x-data-grid";
 import { startCase } from "lodash-es";
-import React, { lazy, useCallback, useEffect, useState } from "react";
+import { lazy, useCallback, useEffect, useState } from "react";
 import { services } from "src/api";
+import { BaseForm } from "src/components/form/BaseForm";
+import { Field } from "src/components/form/Field";
 
 const columnWidths = {
-  "title": 500,
-  "description": 500,
-  "is_hidden": 82,
-  "needs_review": 111,
-  "authors": 125,
-  "producers": 112,
-  "keywords": 500,
-  "subjects": 333,
-  "collection_title": 329,
-  "vol_number": 96,
-  "program": 76,
-  "publishers": 500,
-  "location": 263,
-  "date_string": 97,
-  "notes": 59,
-  "call_number": 100,
-  "generation_item": 91,
-  "format_item": 67,
-  "quality_item": 66,
-  "no_copies": 65,
-  "media_type": 95,
-  "url": 50,
-  "date_created": 176,
-  "creator_name": 152,
-  "date_modified": 176,
-  "contributor_name": 125,
-}
+  title: 500,
+  description: 500,
+  is_hidden: 82,
+  needs_review: 111,
+  authors: 125,
+  producers: 112,
+  keywords: 500,
+  subjects: 333,
+  collection_title: 329,
+  vol_number: 96,
+  program: 76,
+  publishers: 500,
+  location: 263,
+  date_string: 97,
+  notes: 59,
+  call_number: 100,
+  generation_item: 91,
+  format_item: 67,
+  quality_item: 66,
+  no_copies: 65,
+  media_type: 95,
+  url: 50,
+  date_created: 176,
+  creator_name: 152,
+  date_modified: 176,
+  contributor_name: 125,
+};
 
 const columns = [
+
+  {
+    field: "record_id", headerName: "Record ID", renderCell: (params) => (
+      <Link href={`/admin/records/${params.value}`} target="_blank" rel="noreferrer">
+        {params.value}
+      </Link>
+    ),
+  },
   { field: "title" },
   { field: "description" },
   { field: "is_hidden", type: "boolean" },
@@ -81,20 +91,50 @@ const columns = [
   maxWidth: "500",
 }));
 
-const DataGrid = lazy(() => import('@mui/x-data-grid').then(module => ({ default: module.DataGrid })));
-
-// $docs = fetchRows("Select d.docid as 'Document Id', d.Call_Number, c.collection_name as Collection, Title, Authors,
-//     publisher as 'Organization or Publisher', vol_number as 'Vol #-Issue', Day, Month, Year, no_copies as 'No. of Copies', Format, d.Description,
-//     url as 'URL', d.Subjects, d.keywords as Keywords, location as 'Location'
-//     from COLLECTIONS c left join DOCUMENTS d using(collection_id) where d.docid in ($ids) group by docid");
-// const formatItems = (items) => items.map((item) => item?.item).join(", ");
-
+const DataGrid = lazy(() => import("@mui/x-data-grid").then((module) => ({ default: module.DataGrid })));
 const getRowClassName = (params) => (params.indexRelativeToCurrentPage % 2 === 0 ? "even" : "odd");
+
+const processRecordsToRows = (data) => {
+  return data.reduce((acc, item) => {
+    const { media, ...record } = item;
+    media.map((media) => {
+      const { call_number, generation_item, format_item, quality_item, no_copies, media_type, media_id, url } = media;
+
+      const row = {
+        ...record,
+        call_number,
+        generation_item,
+        format_item,
+        quality_item,
+        no_copies,
+        media_type,
+        url,
+        date_created: new Date(record.date_created),
+        date_modified: new Date(record.date_modified),
+        id: media_id,
+      };
+      for (const key of ["format_item", "quality_item", "generation_item", "program"]) {
+        row[key] = record[key]?.item;
+      }
+
+      for (const key of ["date_created", "date_modified"]) {
+        row[key] = record[key] ? new Date(record[key]) : null;
+      }
+      for (const key of ["authors", "producers", "keywords", "subjects", "publishers"]) {
+        row[key] = record[key]?.map((item) => item?.item).join(", ");
+      }
+
+      acc.push(row);
+    });
+    return acc;
+  }, []);
+};
 
 const Table = () => {
   const apiRef = useGridApiRef();
   const [rows, setRows] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [collectionID, setCollectionID] = useState(null);
 
   const fetchData = useCallback(async () => {
     setLoading(true);
@@ -102,97 +142,67 @@ const Table = () => {
       query: {
         $sort: { title: 1 },
         $disable_pagination: true,
-        $select: [
-          ...columns.filter((c) => !c._skipSelect).map((c) => c.field),
-          "media",
-          "record_id",
-        ],
+        $select: [...columns.filter((c) => !c._skipSelect).map((c) => c.field), "media", "record_id"],
+        $limit: Infinity,
+        collection_id: collectionID ? collectionID : undefined,
       },
     });
-    const rows = data.reduce((acc, item) => {
-      const { media, ...record } = item;
-      media.map((media) => {
-        const { call_number, generation_item, format_item, quality_item, no_copies, media_type, media_id, url } = media;
-
-        const row = {
-          ...record,
-          call_number,
-          generation_item,
-          format_item,
-          quality_item,
-          no_copies,
-          media_type,
-          url,
-          date_created: new Date(record.date_created),
-          date_modified: new Date(record.date_modified),
-          id: media_id,
-        }
-        for (const key of ['format_item', 'quality_item', 'generation_item', 'program']) {
-          row[key] = record[key]?.item
-        }
-
-        for (const key of ['date_created', 'date_modified']) {
-          row[key] = record[key] ? new Date(record[key]) : null;
-        }
-        for (const key of ['authors', 'producers', 'keywords', 'subjects', 'publishers']) {
-          row[key] = record[key]?.map((item) => item?.item).join(", ");
-        }
-
-        acc.push(row);
-
-      });
-      return acc;
-    }, []);
-
+    const rows = processRecordsToRows(data);
     setRows(rows);
     setLoading(false);
-  }, []);
+    return rows;
+  }, [collectionID]);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
 
-  // useEffect(() => {
-  //   if (apiRef.current && rows.length) {
-  //     setTimeout(() => {
-  //       apiRef.current.autosizeColumns({
-  //         includeHeaders: true,
-  //         includeOutliers: true,
-  //       });
-  //     }, 1);
-  //   }
-  // }, [rows, apiRef]);
-
-  // useEffect(() => {
-  //   if (apiRef.current && rows.length) {
-  //     setTimeout(() => {
-  //       apiRef.current.autosizeColumns({
-  //         includeHeaders: true,
-  //         includeOutliers: true,
-  //       });
-
-  //       // Log widths after autosizing
-  //       setTimeout(() => {
-  //         const state = apiRef.current.state;
-  //         const widths = {};
-  //         columns.forEach(col => {
-  //           const width = state.columns.columnVisibilityModel[col.field] !== false
-  //             ? state.columns.lookup[col.field]?.computedWidth
-  //             : null;
-  //           if (width) widths[col.field] = Math.round(width);
-  //         });
-  //         console.log('Autosized column widths:', widths);
-  //       }, 100);
-  //     }, 1);
-  //   }
-  // }, [rows, apiRef]);
-
   return (
-    <Box sx={{
-      display: "flex", flexDirection: "column", maxHeight: "100%", '& .MuiDataGrid-row.even': {
-        backgroundColor: '#f9f9f9',
-      },
-    }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        maxHeight: "100%",
+        position: "relative",
+        "& .MuiDataGrid-row.even": {
+          backgroundColor: "#f9f9f9",
+        },
+      }}
+    >
+      <BaseForm
+        formConfig={{
+          service: "records",
+        }}
+      >
+        <Stack
+          direction="row"
+          spacing={3}
+          alignItems={"center"}
+          sx={{
+            position: "absolute",
+            top: "0px",
+            left: "0px",
+            zIndex: 100,
+            width: "500px",
+            minWidth: "100px",
+            height: "auto",
+            px: 1,
+          }}
+        >
+          <Box sx={{ flex: 1, width: "100%" }}>
+            <Field
+              highlightDirty={false}
+              field_type="select"
+              name="collection"
+              service="collections"
+              label="Collection"
+              size="x-small"
+              sx={{ minWidth: 300 }}
+              onChange={(collection) => setCollectionID(collection?.collection_id)}
+            />
+          </Box>
+        </Stack>
+      </BaseForm>
       <DataGrid
         apiRef={apiRef}
         rows={rows}
@@ -203,16 +213,10 @@ const Table = () => {
         showCellVerticalBorder
         showToolbar
         disableRowSelectionOnClick
-        // initialState={{
-        //   pagination: {
-        //     paginationModel: { pageSize: 25, page: 0 },
-        //   },
-        // }}
-        // autosizeOnMount
         getRowClassName={getRowClassName}
       />
-      <Button onClick={() => fetchData()}>Reload Data</Button>
-    </Box >
+      {/* <Button onClick={() => fetchData()}>Reload Data</Button> */}
+    </Box>
   );
 };
 
