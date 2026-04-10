7376b62b616ac8288e606f33d660e9d882dfd015
Greg Michalec <greg+github@primate.net>
2026-04-09 17:01:19 -0700

    simplify createEmotionCache function by removing unnecessary insertion point logic

--- frontend/src/createEmotionCache.js
+++ frontend/src/createEmotionCache.js
@@ -1,14 +1,5 @@
 import createCache from "@emotion/cache";
 
 export default function createEmotionCache() {
-  let insertionPoint = undefined;
-  if (typeof document === "undefined") {
-    return createCache({ key: "css" });
-  }
-  if (typeof document !== "undefined") {
-    const el = document.querySelector('meta[name="emotion-insertion-point"]');
-    insertionPoint = el ?? undefined;
-  }
-
-  return createCache({ key: "css", insertionPoint, prepend: true });
+  return createCache({ key: "css" });
 }
